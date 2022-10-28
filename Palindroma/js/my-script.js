e145deaf9eb0d300e646ae117be3c626562ad625
RaimondoCosma <raimondo.cosma@gmail.com>
2022-10-28 16:54:47 +0200

    Migliora codice esercizio parola palindroma

--- Palindroma/js/my-script.js
+++ Palindroma/js/my-script.js
@@ -2,19 +2,18 @@
 /*-----------------------
     FUNCTIONS
 -----------------------*/
-function isPalindrome(element, concElement, palElement){
-    concElement = '';
+function isPalindrome(element){
+    let concElement = '';
     for ( let i=0; i < element.length; i++) {
-        console.log(element[i]);
         concElement += element[i];
         console.log(concElement);
     }
-    palElement = '';
+    let palElement = '';
     for ( let i=element.length - 1; i >= 0; i--) {
-        console.log(element[i]);
         palElement += element[i];
         console.log(palElement);
     }
+    console.log(`${concElement} ==> ${palElement}`);
     if ( concElement === palElement ) {
         alert('La parola è palindroma');
     } else {
@@ -29,7 +28,9 @@ function isPalindrome(element, concElement, palElement){
 isPalindrome(prompt('Inserisci una parola'));
 
 // Metodo con variabili
+
 // Imposto variabile che chiede l'inserimento di una parola all'utente
 // const word = prompt('Inserisci una parola');
+
 // Imposto variabile che verifichi la funzione
 // const validate = isPalindrome(word);
