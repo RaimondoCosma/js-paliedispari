bbad4a6da26435ca2d836cd62bf428e60b8c7cec
RaimondoCosma <raimondo.cosma@gmail.com>
2022-10-28 17:21:02 +0200

    Aggiunge return true/false alla parola palindroma

--- Palindroma/js/my-script.js
+++ Palindroma/js/my-script.js
@@ -6,31 +6,32 @@ function isPalindrome(element){
     let concElement = '';
     for ( let i=0; i < element.length; i++) {
         concElement += element[i];
-        console.log(concElement);
     }
     let palElement = '';
     for ( let i=element.length - 1; i >= 0; i--) {
         palElement += element[i];
-        console.log(palElement);
     }
     console.log(`${concElement} ==> ${palElement}`);
     if ( concElement === palElement ) {
-        alert('La parola è palindroma');
-    } else {
-        alert('la parola non è palindroma')
-    }
+        return true;
+    } 
+    return false;
 }
 
 /*-----------------------
     MAIN
 -----------------------*/
 // Richiamo direttamente la funzione
-isPalindrome(prompt('Inserisci una parola'));
+// isPalindrome(prompt('Inserisci una parola'));
 
 // Metodo con variabili
 
 // Imposto variabile che chiede l'inserimento di una parola all'utente
-// const word = prompt('Inserisci una parola');
+const word = prompt('Inserisci una parola');
 
 // Imposto variabile che verifichi la funzione
-// const validate = isPalindrome(word);
+if (isPalindrome(word)) {
+    alert('La parola è palindroma!');
+} else {
+    alert('La parola NON è palindroma');
+}
