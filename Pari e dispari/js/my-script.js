b4bc0bfbd64d4bb5283786a59d912c49d915f15e
RaimondoCosma <raimondo.cosma@gmail.com>
2022-10-28 16:32:56 +0200

    Aggiunge esercizio pari-dispari funzionante

--- Pari e dispari/js/my-script.js
+++ Pari e dispari/js/my-script.js
@@ -13,10 +13,33 @@ function isEven(number) {
 // Funzione per generare numeri random dentro un range stabilito
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
-  }
+}
 
 // Funzione per creare la somma tra due numeri
 function sum(num1, num2) {
     let result;
-    result = num1 +num2;
+    return result = num1 +num2;
 }
+
+/*-------------------
+    MAIN
+-------------------*/
+let oddEven = prompt('Scegli tra "pari" e "dispari" e scrivilo nel promp');
+console.log(oddEven);
+
+let userNumber = Number(prompt("Scegli un numero da 1 a 5"));
+console.log(userNumber);
+
+let randomNumBot = getRndInteger(1, 5);
+console.log(randomNumBot);
+
+let total = sum(userNumber, randomNumBot);
+console.log(total)
+
+// Scrivo le condizioni di vittoria
+if ((oddEven === "pari" && isEven(total)) || (oddEven === "dispari" && !isEven(total))){
+    alert('Complimenti hai Vinto!');
+} else {
+    alert('Mi dispiace hai perso!');
+}
+
